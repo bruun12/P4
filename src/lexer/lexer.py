ba100565826582d3d14b07fa6b3e7ec831f02916
Peter Bødstrup <HO77IY@student.aau.dk>
2026-03-23 09:53:50 +0100

    Made changes to Class MyCustomError - Made more test cases for skip comment function

--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -22,6 +22,7 @@ class Token:
 class MyCustomError(Exception):
     def __init__(self, message, error_code):
          super().__init__(message)
+         self.message = message
          self.error_code = error_code
     
     def __str__(self):
--- tests/test_lexer/test_skip_comment.py
+++ tests/test_lexer/test_skip_comment.py
@@ -1,7 +1,32 @@
-from src.lexer.lexer import Lexer
+import pytest
+from src.lexer.lexer import Lexer, MyCustomError
 
 def test_skip_comment_with_inline_comment():
     lexer = Lexer("hello world*/x ")
+
+    lexer.skip_comment()
+    assert lexer.current_char() == 'x'
+
+def test_skip_comment_empty():
+    lexer = Lexer("/**/x")
+    lexer.advance()
+    lexer.advance()
+
+    lexer.skip_comment()
+    assert lexer.current_char() == 'x'
+
+def test_skip_comment_no_end():
+    lexer = Lexer("/* hello world")
+    lexer.advance()
+    lexer.advance()
+
+    with pytest.raises(MyCustomError, match="Comment is never ended"): # Used pytest, for allowing the test to fail
+        lexer.skip_comment()
+
+def test_skip_comment_slash_before_end():
+    lexer = Lexer("/* comment * / still comment */x")
+    lexer.advance()
+    lexer.advance()
 
     lexer.skip_comment()
     assert lexer.current_char() == 'x'
