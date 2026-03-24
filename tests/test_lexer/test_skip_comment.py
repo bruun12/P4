54f5945b1d6917fecc45c4063ca45241a987aa34
Peter Bødstrup <HO77IY@student.aau.dk>
2026-03-24 13:06:34 +0100

    Changed test

--- tests/test_lexer/test_skip_comment.py
+++ tests/test_lexer/test_skip_comment.py
@@ -8,25 +8,19 @@ def test_skip_comment_with_inline_comment():
     assert lexer.current_char() == 'x'
 
 def test_skip_comment_empty():
-    lexer = Lexer("/**/x")
-    lexer.advance()
-    lexer.advance()
+    lexer = Lexer("*/x")
 
     lexer.skip_comment()
     assert lexer.current_char() == 'x'
 
 def test_skip_comment_no_end():
-    lexer = Lexer("/* hello world")
-    lexer.advance()
-    lexer.advance()
+    lexer = Lexer("hello world")
 
     with pytest.raises(MyCustomError, match="Comment is never ended"): # Used pytest, for allowing the test to fail
         lexer.skip_comment()
 
 def test_skip_comment_slash_before_end():
-    lexer = Lexer("/* comment * / still comment */x")
-    lexer.advance()
-    lexer.advance()
+    lexer = Lexer(" comment * / still comment */x")
 
     lexer.skip_comment()
     assert lexer.current_char() == 'x'
