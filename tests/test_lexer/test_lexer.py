4f80674e6c0f70e5f11ca31d437a5dedf8c98c13
8ruun <markusbruunjakobsen@gmail.com>
2026-03-27 10:36:55 +0100

    chore: prepare for lexer test

--- tests/test_lexer/test_lexer.py
+++ tests/test_lexer/test_lexer.py
@@ -1,7 +1,7 @@
 from lexer.lexer import Lexer
 from lexer.lexer import TokenType
 
-def test_lexer():
+def test_lexer_string_declaration():
     lex = Lexer('string x = "Hejsa";')
 
     lex.lexer()
@@ -16,10 +16,41 @@ def test_lexer():
     assert lex.tokens[5].type == TokenType.EOF
     #and some more assertions 
 
-def test_lexer():
-    lex = Lexer('string x = "Hejsa";')
+def test_lexer_integer_assignment():
+    lex = Lexer("""
+                integer i = 1;;
+                i = 9;
+                """)
 
     lex.lexer()
+
+def test_lexer_if_statement():
+    lex = Lexer("""
+                boolean b = True;
+
+                if (b) {
+                    b = False;
+                } else {
+                    b = True;
+                }
+                """)
+
+    lex.lexer()
+
+def test_lexer_while_statement():
+    lex = Lexer("""
+                integer i = 0;
+                integer f = 2; 
+                while (i < 5){
+                    f = f * 2;
+                    i = i + 1;
+                }
+                """)
+
+    lex.lexer()
+
+"""
+    This is inspiration on how to test the lexer
 
     assert lex.tokens[0].type == TokenType.TYPE
     assert lex.tokens[1].type == TokenType.IDENTIFIER
@@ -30,3 +61,4 @@ def test_lexer():
     assert lex.tokens[4].type == TokenType.SEMICOLON
     assert lex.tokens[5].type == TokenType.EOF
     #and some more assertions 
+"""
