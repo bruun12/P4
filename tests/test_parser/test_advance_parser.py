c9211bcbcccd7a0dd989f5162dda6abc8dc68790
Gajoe1 <GM66JO@student.aau.dk>
2026-04-14 12:37:23 +0200

    feat: parser test

--- tests/test_parser/test_advance_parser.py
+++ tests/test_parser/test_advance_parser.py
@@ -2,15 +2,15 @@ from parser.parser import Parser
 from lexer.lexer import Lexer, TokenType
 
 def test_advance():
-    string = "int x = 20 // initilizing x\ny = x * 5"
+    string = "integer x = 20 // initilizing x\ny = x * 5"
     lex = Lexer(string)
 
     lex.lexer()
 
     parser = Parser(lex.tokens)
-    assert parser.current().type == TokenType.IDENTIFIER
+    assert parser.current().type == TokenType.TYPE
 
     while parser.current().type != TokenType.EOF:
         parser.advance()
-    
+
     assert parser.current().type == TokenType.EOF
