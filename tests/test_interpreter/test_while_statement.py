488f58c7244dd2fe56794f5f0dc14c408c68c45d
Katrine123 <113664220+Katrine123@users.noreply.github.com>
2026-04-24 15:30:22 +0200

    extra test of while

--- tests/test_interpreter/test_while_statement.py
+++ tests/test_interpreter/test_while_statement.py
@@ -2,10 +2,30 @@ from parser.parser import Parser
 from lexer.lexer import Lexer
 from parser.ASTNodes import WhileStatement
 
-def test_binary_expression():
+def test_while_statement():
     lex = Lexer("""
                 while(true)
                 {x = 4;}""")
+    lex.lexer()
+    node = Parser(lex.tokens).statement()
+    assert isinstance(node, WhileStatement)
+    assert node.to_c() == "while (true){x = 4;}"
+
+def test_while_statement2():
+    lex = Lexer("""
+                while      (
+                
+                true      )
+
+
+
+                {        x
+                     =
+                
+                    4      ;
+                
+                
+                }""")
     lex.lexer()
     node = Parser(lex.tokens).statement()
     assert isinstance(node, WhileStatement)
