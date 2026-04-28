04037628e410f982f3f2d04510052c798d5c8a1e
Katrine123 <113664220+Katrine123@users.noreply.github.com>
2026-04-28 13:08:32 +0200

    Update test_while_statement.py

--- tests/test_interpreter/test_while_statement.py
+++ tests/test_interpreter/test_while_statement.py
@@ -36,4 +36,40 @@ def test_if_statement2():
     assert correctLines[4] == "{a = (a + b);}"
     assert correctLines[5] == "x = 4;}"
 
+def test_if_statement3():
+    lex = Lexer("""
+                while(true
+                
+                 AND         !
+                
+                
+                false      )
+                
+                
+                {
+                if    (   
+                a       <b){
+                    integer   x 
+                =
+                
+                 8;
+                }       else{
+                a = a + b;  
+
+
+                           }
+                x 
+                 = 4
+                ;}""")
+    lex.lexer()
+    node = Parser(lex.tokens).statement()
+    assert isinstance(node, WhileStatement)
+    correctLines = lineTrim(node.to_c())
+
+    assert correctLines[0] == "while ((true && !false))"
+    assert correctLines[1] == "{if ((a < b))"
+    assert correctLines[2] == "{integer x = 8;}"
+    assert correctLines[3] == "else"
+    assert correctLines[4] == "{a = (a + b);}"
+    assert correctLines[5] == "x = 4;}"
 
