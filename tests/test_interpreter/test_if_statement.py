d915e59a221f9ad9fef175737770ada3b222a51a
Katrine123 <113664220+Katrine123@users.noreply.github.com>
2026-04-28 12:43:35 +0200

    fixed if-statement test

--- tests/test_interpreter/test_if_statement.py
+++ tests/test_interpreter/test_if_statement.py
@@ -1,10 +1,13 @@
 from parser.parser import Parser
 from lexer.lexer import Lexer
 from parser.ASTNodes import IfStatement
-from test_line_trim import lineTrim
+from tests.test_interpreter.test_line_trim import lineTrim
 
 def test_if_statement():
-    correctLines = lineTrim("if(true){}")
+    lex = Lexer("""if(true){}""")
+    lex.lexer()
+    node = Parser(lex.tokens).statement()
+    correctLines = lineTrim(node.to_c())
 
     assert correctLines[0] == "if (true)"
     assert correctLines[1] == "{}"
