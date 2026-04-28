6c232ccc98c2e0049e68e9ec921a8ba0a00ca6e6
Asta Krogsgaard Jensen <asta@krogsgaarden.dk>
2026-04-28 11:24:40 +0200

    LineTrim for test multiple lines + if test

---
+++ tests/test_interpreter/test_if_statement.py
@@ -0,0 +1,10 @@
+from parser.parser import Parser
+from lexer.lexer import Lexer
+from parser.ASTNodes import IfStatement
+from test_line_trim import lineTrim
+
+def test_if_statement():
+    correctLines = lineTrim("if(true){}")
+
+    assert correctLines[0] == "if (true)"
+    assert correctLines[1] == "{}"
