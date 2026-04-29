b58df741f19cbaf7974194c8a5ba550730675430
Asta Krogsgaard Jensen <asta@krogsgaarden.dk>
2026-04-29 08:48:52 +0200

    Update test_if_statement.py

--- tests/test_interpreter/test_if_statement.py
+++ tests/test_interpreter/test_if_statement.py
@@ -3,8 +3,71 @@ from lexer.lexer import Lexer
 from parser.ASTNodes import IfStatement
 from test_line_trim import lineTrim
 
+<<<<<<< Updated upstream
 def test_if_statement():
     correctLines = lineTrim("if(true){}")
 
     assert correctLines[0] == "if (true)"
     assert correctLines[1] == "{}"
+=======
+def test_if_statement_simple():
+    correctLines = lineTrim("""if(true){}""")
+
+    assert correctLines[0] == "if (true)"
+    assert correctLines[1] == "{"
+    assert correctLines[2] == "}"
+
+def test_if_statement_while():
+    correctLines = lineTrim("""if(a<b){x=5;
+                            while(false){
+                            a = a+b;
+                            }
+                            7+5;
+                            }""")
+
+    assert correctLines[0] == "if ((a < b))"
+    assert correctLines[1] == "{"
+    assert correctLines[2] == "x = 5;"
+    assert correctLines[3] == "while (false)"
+    assert correctLines[4] == "{"
+    assert correctLines[5] == "a = (a + b);"
+    assert correctLines[6] == "}"
+    assert correctLines[7] == "(7 + 5);"
+    assert correctLines[8] == "}"
+
+def test_if_statement_else():
+    correctLines = lineTrim("""if(10<9){
+                                   a = a + 7;
+                               }else{
+                                   a = 5;
+                               }""")
+
+    assert correctLines[0] == "if ((10 < 9))"
+    assert correctLines[1] == "{"
+    assert correctLines[2] == "a = (a + 7);"
+    assert correctLines[3] == "}"
+    assert correctLines[4] == "else"  
+    assert correctLines[5] == "{"  
+    assert correctLines[6] == "a = 5;"  
+    assert correctLines[7] == "}"  
+
+def test_if_statement_space():
+    correctLines = lineTrim("""if(          a         ==  14){
+                                   a      = a    + 7  ;
+                               
+                               
+                               
+                               }else{
+                                   a   
+                                   = 5;
+                               }""")
+
+    assert correctLines[0] == "if ((a == 14))"
+    assert correctLines[1] == "{"
+    assert correctLines[2] == "a = (a + 7);"
+    assert correctLines[3] == "}"
+    assert correctLines[4] == "else"  
+    assert correctLines[5] == "{"  
+    assert correctLines[6] == "a = 5;"  
+    assert correctLines[7] == "}" 
+>>>>>>> Stashed changes
