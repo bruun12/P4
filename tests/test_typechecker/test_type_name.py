7c4aa2f5131a1a2695c310874884e96c6b81c727
Asta Krogsgaard Jensen <asta@krogsgaarden.dk>
2026-05-01 15:00:17 +0200

    function with arr fixed to match arrayType

--- tests/test_typechecker/test_type_name.py
+++ tests/test_typechecker/test_type_name.py
@@ -25,8 +25,7 @@ def test_array_type():
     assert type_name(arrayObj_bool) != "boolean"
     assert type_name(arrayObj_void) == "void[]"
     assert type_name(arrayObj_void) != "void" 
-
-#Vi tillader ikke flerdimentionelle arrays       
+    
 def test_nested_array_type():
     arrayObj_nested_int = ArrayType(ArrayType(INTEGER, 1),1)
     arrayObj_nested_str = ArrayType(ArrayType(STRING,1), 1)
@@ -59,26 +58,14 @@ def test_function_tuple_type():
         
     assert type_name(funcObj) == "function(integer, double) -> boolean"
     assert type_name(funcObj) != "function(double, integer) -> string"
-""" 
-Vi tillader ikke parametere til at være arrays
+
 def test_array_function_type():
     funcObj = FunctionType(
-        parameter_types=(ArrayType(INTEGER),),
+        parameter_types=[ArrayType(INTEGER,1),ArrayType(BOOLEAN,1,)],
         return_type=STRING
     )
-    funcObj_str_bool = FunctionType(
-        parameter_types=(ArrayType(STRING),),
-        return_type=BOOLEAN
-    )
-    funcObj_nested = FunctionType(
-        parameter_types=(ArrayType(ArrayType(STRING),),),
-        return_type=BOOLEAN
-    )
+    assert type_name(funcObj) == "function(integer[], boolean[]) -> string"
 
-    assert type_name(funcObj) == "function(integer[]) -> string"
-    assert type_name(funcObj_str_bool) == "function(string[]) -> boolean"
-    assert type_name(funcObj_nested) == "function(string[][]) -> boolean"
-"""
 def test_repr_type():
     tObj = DummyTest()
 
