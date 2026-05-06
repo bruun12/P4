cb60dc43af1df3363c836402d6d6c94169db7947
Asta Krogsgaard Jensen <asta@krogsgaarden.dk>
2026-05-06 13:22:20 +0200

    block statements tests

--- tests/test_typechecker/test_check_statement_blockstmt.py
+++ tests/test_typechecker/test_check_statement_blockstmt.py
@@ -1,125 +1,152 @@
-from type_checker.TypeChecker import TypeChecker, BlockStatement, TypeEnvironment, Statement, NO_FLOW, RETURNS
+from type_checker.TypeChecker import IfStatement, TypeChecker, BlockStatement, TypeEnvironment, ExpressionStatement, Literal
 
-
-class MockStmt(Statement):
+#Collects all call used for the check_statement to campare with expected values
+class SpyChecker(TypeChecker):
     def __init__(self):
-        super().__init__(line=0, column=0)
-    
-    ## Test Block Statement
-def test_empty_block():
-    env = TypeEnvironment(parent=None)
-    stmt = BlockStatement(statements=[], line=0, column=0)
-    checker = TypeChecker(source_code="")
-
-    result = checker.check_statement(stmt, env)
-
-    assert result == NO_FLOW
+        self.call=[]
         
+    def check_statement(self, stmt, env, within_function):
+        self.call.append((stmt, env, within_function)) #Saves the calls happening inside the check_statement
+        super().check_statement(stmt, env, within_function) #Forwards the information to the real check_statement
+
+
+#Test amount of staatement in block statement using within_function
+def test_within_function_in_block_stmt():
+    checker = SpyChecker()
+    exp_stmt = ExpressionStatement(Literal(1, 2, 3), 1, 2) #Random line and column numbers
+    exp_stmt2 = ExpressionStatement(Literal(2, 3, 4), 2, 2)
+           
+    block = BlockStatement([exp_stmt, exp_stmt2], 4,5)
+    env = TypeEnvironment()
     
-def test_block_not_returns():
-    env = TypeEnvironment(parent=None)
-    stmt = BlockStatement(statements=[MockStmt()], line=0, column=0)
-    checker = TypeChecker(source_code="")
+    checker.check_statement(block, env, within_function=True)
+    
+    flags = [call[2] for call in checker.call]
+    
+    outer_flag = flags[0]
+    inner_flag = flags[1:]
+    
+    assert outer_flag == True
+    assert inner_flag == [False, True]
+    
+#Test amount of staatement in block statement using within_function
+def test_within_function_in_block_stmt_type_environment():
+    checker = SpyChecker()
+    exp_stmt = ExpressionStatement(Literal(1, 2, 3), 1, 2) #Random line and column numbers
+    exp_stmt2 = ExpressionStatement(Literal(2, 3, 4), 2, 2)
+           
+    block = BlockStatement([exp_stmt, exp_stmt2], 4,5)
+    env = TypeEnvironment()
+    
+    checker.check_statement(block, env, within_function=True)
+    
+    inner_environment = [call[1] for call in checker.call[1:]] #First env is the block statement
+    
+    for inner_environment in inner_environment:
+        assert inner_environment is not env
+        assert inner_environment.parent is env
 
-    oprindeligCheck = checker.check_statement
-
-    def fake_check_statement(stmt, env):
-        if isinstance(stmt, MockStmt):
-            return NO_FLOW
-        return oprindeligCheck(stmt, env)
-
-    checker.check_statement = fake_check_statement 
-
-    result = checker.check_statement(stmt, env)
-                                             
-    assert result == NO_FLOW
+#Test order of statements in black statament
+def test_within_function_in_block_stmt_stmt_order():
+    checker = SpyChecker()
+    exp_stmt = ExpressionStatement(Literal(1, 2, 3), 1, 2) #Random line and column numbers
+    exp_stmt2 = ExpressionStatement(Literal(2, 3, 4), 2, 2)
+           
+    block = BlockStatement([exp_stmt, exp_stmt2], 4,5)
+    env = TypeEnvironment()
+    
+    checker.check_statement(block, env, within_function=True)
+    stmts_seen = [call[0] for call in checker.call[1:]]
+    assert stmts_seen == [exp_stmt, exp_stmt2]
+    
+#Test to ensure inner_within_function returns false when within_function is false    
+def test_within_function_false_making_inner_within_function_false():
+    checker = SpyChecker()
+    exp_stmt = ExpressionStatement(Literal(1, 2, 3), 1, 2) #Random line and column numbers
+    exp_stmt2 = ExpressionStatement(Literal(2, 3, 4), 2, 2)
+           
+    block = BlockStatement([exp_stmt, exp_stmt2], 4,5)
+    env = TypeEnvironment()
+    
+    checker.check_statement(block, env, within_function=False)
+    
+    inner_flag = [call[2] for call in checker.call[1:]]
+    assert inner_flag == [False, False] #Returns false eventhough the is_last_statment is true
+    
+    
+#Test for nested block statements is in different environments/scopes
+def test_nested_block_stmt():
+    checker = SpyChecker()
+    exp_stmt = ExpressionStatement(Literal(1, 2, 3), 1, 2) #Random line and column numbers
+    exp_stmt2 = ExpressionStatement(Literal(2, 3, 4), 2, 2)
+    
+    if_block = IfStatement(Literal(True, 2, 3), exp_stmt, None, 6, 8) 
+    
+    block = BlockStatement([exp_stmt2, if_block], 4, 5) 
+    env = TypeEnvironment()
         
-
-def test_block_returns():
-    env = TypeEnvironment(parent=None)
-    stmt = BlockStatement(statements=[MockStmt()], line=0, column=0)
-    checker = TypeChecker(source_code="")
-
-    oprindeligCheck = checker.check_statement
-
-    def fake_check_statement(stmt, env):
-        if isinstance(stmt, MockStmt):
-            return RETURNS
-        return oprindeligCheck(stmt, env)
-
-    checker.check_statement = fake_check_statement 
-
-    result = checker.check_statement(stmt, env)
-                                             
-    assert result == RETURNS
+    checker.check_statement(block, env, within_function=True)
     
-
-def test_more_stmts_last_block_returns():
-    env = TypeEnvironment(parent=None)
-    checker = TypeChecker(source_code="")
-
-    stmt1 = MockStmt()
-    stmt2 = MockStmt()
-    stmt3 = MockStmt()
-
-    stmt = BlockStatement(statements=[stmt1, stmt2, stmt3], line=0, column=0)
-
-    call_count = 0
-
-    oprindeligChecker = checker.check_statement
-
-    def fake_check_stmt(stmt, env):
-        nonlocal call_count
-
-        if isinstance(stmt, MockStmt):
-            call_count += 1
-            if call_count < 3:
-                return NO_FLOW
-            else:
-                return RETURNS
-        return oprindeligChecker(stmt, env)
+    calls = checker.call
+    
+    #envs created. First [] = which call (stmt, env, flag/bool), second [] = which field in the call ([1] = env)
+    outer_env = calls[0][1]
+    block_env = calls[1][1]
+    if_env = calls[3][1]
+    
+    exp_stmt2_scope =  calls[2][1]
+    
+    # block_stmt is in the outer env and therefore create a new
+    assert block_env is not outer_env
+    assert block_env.parent is outer_env
+    
+    assert exp_stmt2_scope is block_env
+    
+    # if_stmt is in the block_env and also create a new if_env (containing the then_branch)
+    assert if_env is not block_env
+    assert if_env.parent is block_env
+    
+#Check that the outmost blocktament is has None as the environment
+def test_block_stmt_parent_is_None():
+    checker = SpyChecker()
+    exp_stmt = ExpressionStatement(Literal(1, 2, 3), 1, 2) #Random line and column numbers
+    exp_stmt2 = ExpressionStatement(Literal(4, 2, 3), 1, 2)
         
-    checker.check_statement = fake_check_stmt
-
-    result = checker.check_statement(stmt, env)
-
-    assert result == RETURNS
-
-    
-def test_test_more_stmts_first_block_returns():
-    env = TypeEnvironment(parent=None)
-    checker = TypeChecker(source_code="")
-
-    stmt1 = MockStmt()
-    stmt2 = MockStmt()
-    stmt3 = MockStmt()
-
-    stmt = BlockStatement(statements=[stmt1, stmt2, stmt3], line=0, column=0)
-
-    call_count = 0
-
-    oprindeligChecker = checker.check_statement
-
-    def fake_check_stmt(stmt, env):
-        nonlocal call_count
-
-            # det her er forkert - den skal starte med at kigge på den første, returner den, og så tage de to næste uden af returnerer
-            # men den skal ikke køre mere end det fordi så looper den ik? 
-        if isinstance(stmt, MockStmt):
-            call_count += 1 
-            if call_count > 0:
-                return NO_FLOW
-            else:
-                return RETURNS
-        return oprindeligChecker(stmt, env)
+    block = BlockStatement([exp_stmt, exp_stmt2], 4, 5)  
+    env = TypeEnvironment()
         
-    checker.check_statement = fake_check_stmt
-
-    result = checker.check_statement(stmt, env)
-
-    assert result == RETURNS
+    checker.check_statement(block, env, within_function=True)
     
-''' 
-    def test_unreacable_block_stmt(self):
-'''
+    calls = checker.call
     
+    #envs created. First [] = which call (stmt, env, flag/bool), second [] = which field in the call ([1] = env)
+    outer_env = calls[0][1]
+    
+    # block_stmt is in the outer env and therefore create a new
+    assert outer_env.parent is None
+ 
+#Test empty blocks creates envs but is allowed   
+def test_block_stmt_empty():
+    checker = SpyChecker()
+         
+    block2 = BlockStatement([], 3, 6)
+    block3 = BlockStatement([], 3, 6)
+    outer_block = BlockStatement([block2, block3], 4, 5)  
+    env = TypeEnvironment()
+        
+    checker.check_statement(outer_block, env, within_function=True)
+    
+    calls = checker.call
+    
+    #envs created. First [] = which call (stmt, env, flag/bool), second [] = which field in the call ([1] = env)
+    outer_env = calls[0][1]
+    block2_env = calls[1][1]
+    block3_env = calls[2][1]    
+    assert outer_env.parent is None    
+    
+    # block_stmt is in the outer env and therefore create a new
+    assert block2_env.parent is outer_env
+    assert block2_env is not outer_env
+    
+    assert block3_env.parent is outer_env
+    assert block3_env is not outer_env
