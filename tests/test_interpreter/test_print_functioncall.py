2320f1f1bbff587c419f9204eca668e93a18d7cf
Katrine123 <113664220+Katrine123@users.noreply.github.com>
2026-05-19 14:22:52 +0200

    the print tests are fixed

--- tests/test_interpreter/test_print_functioncall.py
+++ tests/test_interpreter/test_print_functioncall.py
@@ -5,11 +5,16 @@ from parser.ASTNodes import BlockStatement
 from tests.test_interpreter.test_line_trim import lineTrim
 
 def test_print_func1():
-    lex = Lexer("""print("hej med dig");""")
-    lex.lexer()
-    node = Parser(lex.tokens).parse_expression()
-    assert isinstance(node, FunctionCall)
-    assert node.to_c() == """if(sizeof("hej med dig")==8){printf("%f","hej med dig");}else if(sizeof("hej med dig")==4){printf("%d","hej med dig");}else if(sizeof("hej med dig")==1){printf("%s","hej med dig" ? "true" : "false");}else{printf("%s","hej med dig");}"""
+    correctLines = lineTrim("""print("hej med dig");""")
+    assert correctLines[0] == """if (sametypeof(1.2,"hej med dig")){"""
+    assert correctLines[1] == """printf("%f","hej med dig");"""
+    assert correctLines[2] == """} else if (sizeof("hej med dig") == 1) {"""
+    assert correctLines[3] == """printf("%s", "hej med dig" ? "true" : "false");"""
+    assert correctLines[4] == """} else if (sametypeof(1, "hej med dig")){"""
+    assert correctLines[5] == """printf("%d", "hej med dig");"""
+    assert correctLines[6] == """} else if (sametypeof("string","hej med dig")){"""
+    assert correctLines[7] == """printf("%s","hej med dig");"""
+    assert correctLines[8] == """}"""
 
 def test_print_func2():
     correctLines = lineTrim("""
@@ -20,8 +25,26 @@ def test_print_func2():
     assert correctLines[0] == "{"
     assert correctLines[1] == "int x = 5;"
     assert correctLines[2] == "double a = 3.2;"
-    assert correctLines[3] == """if(sizeof(x)==8){printf("%f",x);}else if(sizeof(x)==4){printf("%d",x);}else if(sizeof(x)==1){printf("%s",x ? "true" : "false");}else{printf("%s",x);}if(sizeof(a)==8){printf("%f",a);}else if(sizeof(a)==4){printf("%d",a);}else if(sizeof(a)==1){printf("%s",a ? "true" : "false");}else{printf("%s",a);};"""
-    assert correctLines[4] == "}"
+    assert correctLines[3] == """if (sametypeof(1.2,x)){"""
+    assert correctLines[4] == """printf("%f",x);"""
+    assert correctLines[5] == """} else if (sizeof(x) == 1) {"""
+    assert correctLines[6] == """printf("%s", x ? "true" : "false");"""
+    assert correctLines[7] == """} else if (sametypeof(1, x)){"""
+    assert correctLines[8] == """printf("%d", x);"""
+    assert correctLines[9] == """} else if (sametypeof("string",x)){"""
+    assert correctLines[10] == """printf("%s",x);"""
+    assert correctLines[11] == """}"""
+    assert correctLines[12] == """if (sametypeof(1.2,a)){"""
+    assert correctLines[13] == """printf("%f",a);"""
+    assert correctLines[14] == """} else if (sizeof(a) == 1) {"""
+    assert correctLines[15] == """printf("%s", a ? "true" : "false");"""
+    assert correctLines[16] == """} else if (sametypeof(1, a)){"""
+    assert correctLines[17] == """printf("%d", a);"""
+    assert correctLines[18] == """} else if (sametypeof("string",a)){"""
+    assert correctLines[19] == """printf("%s",a);"""
+    assert correctLines[20] == """}"""
+    assert correctLines[21] == ";"
+    assert correctLines[22] == "}"
 
 def test_print_func3():
     correctLines = lineTrim("""{
@@ -30,5 +53,14 @@ def test_print_func3():
                 }""")
     assert correctLines[0] == "{"
     assert correctLines[1] == "bool b = true;"
-    assert correctLines[2] == """if(sizeof(b)==8){printf("%f",b);}else if(sizeof(b)==4){printf("%d",b);}else if(sizeof(b)==1){printf("%s",b ? "true" : "false");}else{printf("%s",b);};"""
-    assert correctLines[3] == "}"
+    assert correctLines[2] == """if (sametypeof(1.2,b)){"""
+    assert correctLines[3] == """printf("%f",b);"""
+    assert correctLines[4] == """} else if (sizeof(b) == 1) {"""
+    assert correctLines[5] == """printf("%s", b ? "true" : "false");"""
+    assert correctLines[6] == """} else if (sametypeof(1, b)){"""
+    assert correctLines[7] == """printf("%d", b);"""
+    assert correctLines[8] == """} else if (sametypeof("string",b)){"""
+    assert correctLines[9] == """printf("%s",b);"""
+    assert correctLines[10] == """}"""
+    assert correctLines[11] == ";"
+    assert correctLines[12] == "}"
