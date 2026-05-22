162bb73dba1e8f343d8890e91fb3097d4e599a5a
miagj02 <mjosef24@student.aau.dk>
2026-05-22 13:27:52 +0200

    Test: extra testing to advance(), current_char(), peek_char(), and read_word() in lexer

--- tests/test_lexer/test_advance.py
+++ tests/test_lexer/test_advance.py
@@ -1,6 +1,6 @@
 from lexer.lexer import Lexer
 
-# checks if it can advance one line
+# Checks if it can advance one line
 def test_advance_one_line():
     string = "int x = 20 // initilizing x\ny = x * 5"
     lex = Lexer(string)
@@ -8,6 +8,17 @@ def test_advance_one_line():
     while(lex.peek_next_char() is not None):
         lex.advance()
 
+    assert not lex.line == 1
     assert lex.line == 2
     assert lex.column == 9
 
+# Checks it cannot advance to a line and column that does not exist
+def test_cannot_advance():
+    string = "int x = 20 // initilizing x\ny = x * 5"
+    lex = Lexer(string)
+
+    while(lex.current_char() is not None):
+        lex.advance()
+
+    assert lex.advance() is None
+    assert not lex.line == 3 # to explicit show it does not take the next line
--- tests/test_lexer/test_peek_char.py
+++ tests/test_lexer/test_peek_char.py
@@ -5,3 +5,25 @@ def test_peek_next_char():
     string = "int x = 20 // initilizing x"
     lex = Lexer(string)
     assert lex.peek_next_char() == "n"
+
+# Checks it does not peek two chars at the same time
+def test_not_peek_two_char():
+    string = "int x = 20 // initilizing x"
+    lex = Lexer(string)
+    assert not lex.peek_next_char() == "nt"
+
+# Checks if it correctly stops peeking the next line if there is none
+def test_stop_peek_next_char():
+    string = "int x = 20 // initilizing x"
+    lex = Lexer(string)
+
+    while(lex.peek_next_char() is not None):
+        lex.advance()
+
+    assert lex.peek_next_char() is None
+
+# Checks it can peek a space
+def test_peek_space():
+    string = "  int x = 20 // initilizing x"
+    lex = Lexer(string)
+    assert lex.peek_next_char() == " "
