fb4c65c68384a5cfffed24c21803b1d986ae34d6
bruun12 <markusbruunjakobsen@gmail.com>
2026-05-25 10:13:06 +0000

    chore: help messages updated to fix executable instead of main.py

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ Optionals:
     -k      Keeps the transpiled c-file followed by c-file example: -k output.c
               
 Example:
-python main.py cimple.cimple exec -r -k exec.c
+cimple cimple.cimple exec -r -k exec.c
 """, file=sys.stdout)
         sys.exit(0) 
     
@@ -59,14 +59,14 @@ python main.py cimple.cimple exec -r -k exec.c
             with open(sys.argv[1], "r", encoding="utf-8") as file: # read the file parsed - "r" = read
                 source = file.read()
         except FileNotFoundError:
-            print("No file is found.\n\nTo get help:\npython main.py --help", file=sys.stderr)
+            print("No file is found.\n\nTo get help:\ncimple --help", file=sys.stderr)
             sys.exit(1)
 
         if source.strip() is "":
             print("The provided file is empty. Try another file", file=sys.stderr)
             sys.exit(ErrorCode.EMPTY_SOURCE_ERROR.value)
     else:
-        print("not the valid arguments. \n\nTo get help:\npython main.py --help")
+        print("not the valid arguments. \n\nTo get help:\ncimple --help")
         sys.exit(ErrorCode.ARGUMENT_ERROR.value)
 
     try:  
